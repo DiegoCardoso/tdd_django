bbc4468a23bc2413271f9ff8d0e78be7a4d50441
diegocardoso <diegopc86@gmail.com>
2014-10-07 18:55:07 -0300

    Front page HTML generated from a template pass in more tests.

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wa	it(3)
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.getattribute('placeholder'), 'Enter a to-do item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
@@ -35,8 +35,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
-		rowa = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
