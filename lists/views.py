3d268253978a97a9969e40c1b308ac6fbec3d505
diegocardoso <diegopc86@gmail.com>
2014-10-08 12:43:10 -0300

    Now handles the form submit by post

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
+
+	if request.method == 'POST':
+		return render(request, 'home.html', { 'new_item_text': request.POST.get('item_text', '')})
+
 	return render(request, 'home.html')
