e43b1fd1f9bfc90f2c498aa91569ebbc6524e0d4
Diego Cardoso <diegopc86@gmail.com>
2014-10-11 05:28:53 +0200

    Preparing the separate lists for users

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -21,16 +21,25 @@ class HomePageTest(TestCase):
 		self.assertEqual(response.content.decode(), expected_html)	
 		
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 	
 	def test_saving_and_retrieving_items(self):
+
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item, the second'
+		second_item.list = list_		
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -38,7 +47,10 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)		
 		self.assertEqual(second_saved_item.text, 'Item, the second')
+		self.assertEqual(second_saved_item.list, list_)		
+
 
 class ListViewTest(TestCase):
 	
@@ -47,8 +59,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 		
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -61,7 +74,8 @@ class NewListTest(TestCase):
 		
 		self.client.post(
 			'/lists/new',
-			data={'item_text': 'A new list item'})		
+			data={'item_text': 'A new list item'}
+			)		
 
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
