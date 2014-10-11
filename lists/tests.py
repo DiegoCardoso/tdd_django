175fc1a66484af79f5f71ecc792592fe4ad4f9c4
Diego Cardoso <diegopc86@gmail.com>
2014-10-11 05:10:19 +0200

    Added new URL to create a new list item via POST

--- lists/tests.py
+++ lists/tests.py
@@ -18,36 +18,7 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
-
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
-
-	def test_home_page_redirects_after_POST(self):		
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-
-
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(), 0)
-
-	
+		self.assertEqual(response.content.decode(), expected_html)	
 		
 
 class ItemModelTest(TestCase):
@@ -83,3 +54,22 @@ class ListViewTest(TestCase):
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+
+	def test_saving_a_POST_request(self):
+		
+		self.client.post(
+			'/lists/new',
+			data={'item_text': 'A new list item'})		
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):		
+		response = self.client.post(
+			'/lists/new',
+			data={'item_text': 'A new list item'})		
+
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
