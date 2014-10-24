d874efc2d24750dbc1b0d655baa4d70c61c24880
Diego Cardoso <diegopc86@gmail.com>
2014-10-24 04:19:51 +0200

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,25 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
 	
-	def setUp(self):
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			pass
+
+	def setUp(self):		
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(5)
 
@@ -21,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -65,7 +79,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		#Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -90,7 +104,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		#She notices the input box nicely centered
--- superlists/urls.py
+++ superlists/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^lists/', include('lists.urls')),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
